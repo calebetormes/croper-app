74e53d301cf7e8e3b0ef609910cd3240a30da5bd
calebetormes <calebetormes@gmail.com>
2025-08-01 11:15:47 -0300

    fix: remove default value for 'gerente_id' select field and make it searchable

--- app/Filament/Resources/NegociacaoResource/Forms/Sections/BasicInformationSectionForm.php
+++ app/Filament/Resources/NegociacaoResource/Forms/Sections/BasicInformationSectionForm.php
@@ -132,7 +132,7 @@ class BasicInformationSectionForm
                     ->disabled(fn() => auth()->user()?->role?->name === 'Vendedor')
                     ->searchable()
                     ->required()
-                    ->default(1)
+                    //->default(1)
                     ->dehydrated(),
             ])
             ->columns(3);
